a78a90368516bbd3f6e3358dba8a424cbbeb8e11
Saiem Solimullah <saiemsolimullah@gmail.com>
2025-07-24 09:49:56 +0200

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,3 @@
+
 #!/bin/bash
-
-# Exit on any error
-set -e
-
-# Start FastAPI server using Uvicorn
-exec uvicorn app.main:app --host 0.0.0.0 --port 5000
+uvicorn app.main:app --host 0.0.0.0 --port 5000
